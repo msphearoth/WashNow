3b2c56b0467c4e046f601a33c3cfde16c5cc33b8
phearoth <msphearoth@gmail.com>
2017-07-18 14:53:26 +0700

    Home's UI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,39 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  notification: any;
   constructor(public navCtrl: NavController) {
+    this.getNotification();
+  }
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
+  getNotification() {
+    if(localStorage.getItem('notification') == null) {
+      this.notification = 'notifications-off';
+    } else {
+      this.notification = localStorage.getItem('notification');
+    }
+  }
+
+  setNotification() {
+    if(localStorage.getItem('notification') == null) {
+      localStorage.setItem('notification', 'notifications-off');
+    } else {
+      if(localStorage.getItem('notification') == 'notifications-off') {
+        localStorage.setItem('notification', 'notifications');
+      } else {
+        localStorage.setItem('notification', 'notifications-off');
+      }
+    }
+    this.getNotification();
   }
 
 }
